60c7069cf993756a3b2b47d50ea7c9a299bf97e2
Adit2908 <Adit2908>
2026-07-13 11:00:55 +0530

    exploring get,post,delete

--- homework.md
+++ homework.md
@@ -8,4 +8,10 @@
 - difference between caret and tilde
 
 - initialize git 
-gitignore
+.gitignore
+- Create a remote  repo on github
+- Push all code to remote origin
+- Play with routes and route extensions ex. /hello, /, hello/2
+- Order of the routes matter a lot
+- Install Postman app and make a workspace/collection > test API call
+- Write logic to handle GET,POST,PATCH,DELETE API Calls and test them onn Post
--- src/app.js
+++ src/app.js
@@ -2,22 +2,34 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user",(req,res)=>{
+  res.send("HAHAHAHAHAHAHHAHAAHAHA")
+})
+
+// This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Aditya",lastName:"Pratap Rao"})
+})
+
+
+app.post("/user",(req,res)=>{
+//saving data to DB
+  res.send("Data Successfully saved to the database")
+})
+
+
+app.delete("/user",(req,res)=>{
+//saving data to DB
+  res.send("Deleted successfully")
+})
+
 //middleware-middleware is a function that executes during the request-response lifecycle of your application
 
 
-app.use("/hello",(req, res) => {
-  res.send("Namaste Jhulelaluuu");
-});
-
+//this will match all the HTTP method method API calls to /test
 app.use("/test",(req, res) => {
   res.send("my server created successfully");
 });
-
-app.use("/",(req, res) => {
-  res.send("hello from the dashboard");
-});
-
-
 
 
 app.listen(7777, () => {
