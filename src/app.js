6b3ba7e999f109d8973bcfc30139c15cd7c1bacc
Adit2908 <Adit2908>
2026-08-21 00:16:06 +0530

    cors origin gets  updated

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
        "http://localhost:5173",
-      "https://dev-tinder-web-neon-rho.vercel.app",
+      "https://dev-tinder-web-aditya34.vercel.app",
     ],
      
     
