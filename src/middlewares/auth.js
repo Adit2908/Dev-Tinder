5b231f26558ad6b35fdab846b4d6ec11e393552b
Adit2908 <Adit2908>
2026-08-21 00:42:19 +0530

    fix: clean up dead code and use process.env.JWT_SECRET consistently

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,81 +1,18 @@
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/user");
-
-// const userAuth = async (req, res, next) => {
-//   try {
-//     const {token} = req.cookies;
-//     if (!token) {
-//       return res.status(401).send("Please Login!");
-//     }
-
-//     const decodedObj = await jwt.verify(token, "DEV@Tinder$678");
-
-//     const { _id } = decodedObj;
-
-//     const user = await User.findById(_id);
-
-//     if (!user) {
-//       throw new Error("user not foundz");
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (err) {
-//     res.status(400).send("ERROR: " + err.message);
-//   }
-//   //Read the token from the req.cookies
-
-//   //Find the username
-// };
-
-// module.exports = { userAuth };
-
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/user");
-
-// const userAuth = async (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization;
-//     const token = authHeader && authHeader.startsWith("Bearer ")
-//       ? authHeader.split(" ")[1]
-//       : null;
-
-//     if (!token) {
-//       return res.status(401).send("Please Login!");
-//     }
-
-//     const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
-
-//     const { _id } = decodedObj;
-
-//     const user = await User.findById(_id);
-
-//     if (!user) {
-//       throw new Error("user not found");
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (err) {
-//     res.status(400).send("ERROR: " + err.message);
-//   }
-// };
-
-// module.exports = { userAuth };
-
-
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
-    
+
     if (!token) {
       return res.status(401).send("Please Login!");
     }
 
-    const decodedObj = jwt.verify(token, process.env.JWT_SECRET || "DEV@Tinder$678");
+    const decodedObj = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "DEV@Tinder$678"
+    );
     const { _id } = decodedObj;
 
     const user = await User.findById(_id);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -19,9 +19,9 @@ authRouter.post("/signup", async (req, res) => {
       photoUrl,
       about,
     } = req.body;
+
     //Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
 
     //Creating a new instance of the User model
     const user = new User({
@@ -39,10 +39,6 @@ authRouter.post("/signup", async (req, res) => {
     const token = await savedUser.getJWT();
 
     //Add the token to cookie and send the response to the user
-    // res.cookie("token", token, {
-    //   expires: new Date(Date.now() + 8 * 3600000),
-    // });
-
     res.cookie("token", token, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
@@ -56,75 +52,11 @@ authRouter.post("/signup", async (req, res) => {
   }
 });
 
-// authRouter.post("/login", async (req, res) => {
-//   try {
-//     const { emailId, password } = req.body;
-
-//     const user = await User.findOne({ emailId: emailId });
-//     if (!user) {
-//       throw new Error("Invalid Credential");
-//     }
-
-//     const isPasswordValid = await user.validatePassword(password);
-//     if (isPasswordValid) {
-//       //Create a JWT token
-
-//       const token = await user.getJWT();
-
-//       //Add the token to cookie and send the response to the user
-//       res.cookie("token", token, {
-//         expires: new Date(Date.now() + 8 * 3600000),
-//       });
-//       res.send(user);
-//     } else {
-//       throw new Error("Invalid Credential");
-//     }
-//   } catch (error) {
-//     res.status(400).send("Error : " + error.message);
-//   }
-// });
-
-// authRouter.post("/login", async (req, res) => {
-//   try {
-//     const { emailId, password } = req.body;
-
-//     const user = await User.findOne({ emailId });
-
-//     if (!user) {
-//       throw new Error("Invalid Credential");
-//     }
-
-//     const isPasswordValid = await user.validatePassword(password);
-
-//     if (!isPasswordValid) {
-//       throw new Error("Invalid Credential");
-//     }
-
-//     const token = await user.getJWT();
-
-//     res.cookie("token", token, {
-//       maxAge: 8 * 60 * 60 * 1000,
-//       httpOnly: true,
-//       sameSite: "lax",
-//       secure: false,
-//     });
-
-//     res.send(user);
-//   } catch (error) {
-//     res.status(400).send("Error : " + error.message);
-//   }
-// });
-
 authRouter.post("/login", async (req, res) => {
   try {
-    console.log("LOGIN REQUEST RECEIVED");
-    console.log("Email:", req.body.emailId);
-
     const { emailId, password } = req.body;
 
     const user = await User.findOne({ emailId });
-
-    console.log("USER FOUND:", !!user);
 
     if (!user) {
       throw new Error("Invalid Credential");
@@ -132,15 +64,11 @@ authRouter.post("/login", async (req, res) => {
 
     const isPasswordValid = await user.validatePassword(password);
 
-    console.log("PASSWORD VALID:", isPasswordValid);
-
     if (!isPasswordValid) {
       throw new Error("Invalid Credential");
     }
 
     const token = await user.getJWT();
-
-    console.log("TOKEN CREATED:", !!token);
 
     res.cookie("token", token, {
       maxAge: 8 * 60 * 60 * 1000,
@@ -149,22 +77,20 @@ authRouter.post("/login", async (req, res) => {
       sameSite: "none",
     });
 
-    console.log("COOKIE SET");
-
     res.send(user);
   } catch (error) {
-    console.log("LOGIN ERROR:", error);
     res.status(400).send("Error : " + error.message);
   }
 });
 
 authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
-  expires: new Date(Date.now()),
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-});
+    expires: new Date(Date.now()),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
   res.send("logout succesfull");
 });
+
 module.exports = authRouter;
